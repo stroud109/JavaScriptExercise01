668a7d591016b5e8ddbe376994dd0004bfdb68c4
Hackbright Student <user@mini-004.(none)>
2013-10-17 16:26:38 -0700

    Changed variables, attributes

--- readingList.js
+++ readingList.js
@@ -1,34 +1,52 @@
-
-
-var BookList = function(numRead, numNotRead, nextBook, currentBook, lastBook, bookShelf) {
-    this.numRead = numRead;
-    this.numNotRead = numNotRead;
-    this.nextBook = nextBook;
-    this.currentBook = currentBook;
-    this.lastBook = lastBook;
-    this.bookShelf = [numRead, numNotRead, nextBook, currentBook, lastBook];
-
-    this.add(book) = function() {
-        this.bookShelf.push(book);
-    }
-
-    this.finishCurrentBook = function() {
-
-    }
-
-};
-
 var Book = function(titleBook, genre, author, read, readDate) {
     this.titleBook = titleBook;
     this.genre = genre;
     this.author = author;
     this.read = read;
     this.readDate = readDate;
-    }
+    };
 
- 
+var BookList = function() {
+    this.booksRead = [];
+    this.booksNotRead = [];
+    this.numRead = booksRead.length; // use len on a list
+    this.numNotRead = booksNotRead.length;
+    this.nextBook = booksNotRead[0];
+    this.lastBook = booksRead[booksRead.length-1];
+    this.bookShelf = [];
+
+    this.addBook = function(book) {
+        this.bookShelf.push(book);
+        console.log("This book has been successfully added.");
+        console.log("There are now " + bookShelf.length + "books on your bookshelf.");
+
+        if (book.read === true) {
+            this.booksRead.push(book);
+        }
+        else
+            this.booksNotRead.push(book);
+    };
+
+    this.finishCurrentBook = function() {
+
+    };
+
 };
 
-var HarryPotter = new Book("Sorcerer's Stone", "fantasy", "J.K. Rowling", true, "10/16/2013");
 
-        
+var harryPotter = new Book("Sorcerer's Stone", "fantasy", "J.K. Rowling", true, "10/16/2013");
+
+var cleopatra = new Book("Cleopatra: A Life", "biography", "Stacy Schiff", true, "10/1/2012");
+
+var pridePrejudice = new Book("Pride and Prejudice", "classics", "Jane Austin", false, null);
+
+var penelopeDaughter = new Book("Penelope's Daughter", "historical fiction", "Laurel Corona", true, "12/5/2009");
+
+var hobbit = new Book("The Hobbit", "fantasy", "J.R.R. Tolkien", false, null);
+
+var stephList = new BookList();
+// var jeeList = new BookList(numRead, numNotRead, nextBook, currentBook, lastBook, bookShelf);
+
+// var mylist = new BookList();
+stephList.addBook(hobbit);
+console.log(BookList);
